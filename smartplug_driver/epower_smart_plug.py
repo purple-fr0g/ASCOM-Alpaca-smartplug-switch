63c112ee0899ea1fe1005e3952668d8ed6e02648
Dominik <tatrix42@gmail.com>
2026-03-07 14:12:03 +0100

    fixes from on site testing

--- smartplug_driver/epower_smart_plug.py
+++ smartplug_driver/epower_smart_plug.py
@@ -4,13 +4,13 @@ import re
 
 POWERSWITCHLEN = 4
 
-async def switch_power(addr: str, outlet_index: int, to_state: bool) -> None:
-    response = get(f"http://{addr}/hidden.htm?M0:{outlet_index}={"ON" if to_state else "OFF"}")
+def switch_power(addr: str, outlet_index: int, to_state: bool) -> None:
+    response = get(f"http://{addr}/hidden.htm?M0:O{outlet_index+1}={"ON" if to_state else "OFF"}")
 
     response.raise_for_status()
     return None
 
-async def get_power_status(addr: str) -> list[bool]:
+def get_power_status(addr: str) -> list[bool]:
     response = get(f"http://{addr}/hidden.htm")
     response.raise_for_status()
     plugs = list()
@@ -20,6 +20,6 @@ async def get_power_status(addr: str) -> list[bool]:
         match = re.match(pattern, line.decode('utf-8'))
         if match:
             status = match.group(2)
-            plugs.append(True if status == "ON" else False)
+            plugs.append(True if status == "On" else False)
 
     return plugs
